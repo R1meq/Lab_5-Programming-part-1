9475e8a482aa74d5952da7b2d0d546d7ee8568bf
R1meq <romansenyk123@gmail.com>
2022-12-01 04:53:32 +0200

    Declaration class Land

---
+++ Land.h
@@ -0,0 +1,24 @@
+﻿#ifndef _LAND_
+#define _LAND_
+#include "Country.h"
+#include <string>
+#include <list>
+
+class Land
+{
+private:
+	string name;
+	list<Country> listCountries;
+public:
+	Land();
+	Land(string name, list<Country> listCountries);
+	void setName(string name);
+	string getName() const;
+	void sortByGPD();
+	void printLand();
+	void printLandTopByGPD(const int &count);
+	Country TopByPopulation(governmentType type) const;
+
+};
+
+#endif // _LAND_
